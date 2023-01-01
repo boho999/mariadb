52893cb6160f0f7e8968506aa53ed44f22597959
boho999 <boho999@hotmail.com>
2023-01-01 19:24:49 +0100

    Working app with hashed password

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pool = require('../helpers/database');
 const router = express.Router();
+const bcrypt = require("bcrypt");
 
 router.get('/:id', async function(req,res) {
 
@@ -10,7 +11,7 @@ router.get('/:id', async function(req,res) {
         res.status(200).json(rows);
 
     } catch (error) {
-        res.status(400).send(error.message)
+        res.status(400).send(error.message);
     }
     
 })
@@ -19,13 +20,29 @@ router.post('/register', async function(req,res){
     try {
         const {email, password} = req.body;
         console.log(email, password);
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
         const sqlQuery= 'INSERT INTO user (email, password) VALUES (?,?)';
-        const result = await pool.query(sqlQuery,[email, password]);
+        const result = await pool.query(sqlQuery,[email, encryptedPassword]);
 
         res.status(200).json({userid: result.insertId.toString()});
- 
+
     } catch (error) {
-        res.status(400).send(error.message)
+        res.status(400).send(error.message);
+    }
+})
+
+router.post('/login', async function(req,res){
+    try {
+        const {email, password} = req.body;
+        
+        const sqlQuery = 'SELECT password from user WHERE email = ?';
+        
+        const result = await pool.query(sqlQuery, [email]);
+        const isValid = await bcrypt.compare(password, result[0].password);        
+        res.status(200).json({valid: isValid});
+    } catch (error) {
+        res.status(400).send(error.message);
     }
 })
 
